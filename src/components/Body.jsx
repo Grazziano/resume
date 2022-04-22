fbc197a9646592c56e24b5b424b08886690a920a
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-04-21 21:15:32 -0300

    Body Component

---
+++ src/components/Body.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import About from '../pages/About';
+import Awards from '../pages/Awards';
+import Education from '../pages/Education';
+import Experience from '../pages/Experience';
+import Interests from '../pages/Interests';
+import Skills from '../pages/Skills';
+
+export default function Body() {
+  return (
+    <div className="container-fluid p-0">
+      {/* About */}
+      <About />
+      <hr className="m-0" />
+      {/* Experience */}
+      <Experience />
+      <hr className="m-0" />
+      {/* Education */}
+      <Education />
+      <hr className="m-0" />
+      {/* Skills */}
+      <Skills />
+      <hr className="m-0" />
+      {/* Interests */}
+      <Interests />
+      <hr className="m-0" />
+      {/* Awards */}
+      <Awards />
+    </div>
+  );
+}
