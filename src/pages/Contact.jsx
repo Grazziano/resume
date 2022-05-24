1d08d6d87da70892fd8e335f162f19a3ed268ae4
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-05-23 22:18:18 -0300

    Contact Me Form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    const values = {
+      name,
+      email,
+      message,
+    };
+    console.log(values);
+  }
+
   return (
     <section className="resume-section" id="contact">
       <div className="resume-section-content">
         <h2 className="mb-5">Contact Me</h2>
 
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div class="mb-3">
             <label for="name" class="form-label">
               Name
             </label>
-            <input type="text" class="form-control" id="name" />
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              name="name"
+              onChange={({ target }) => setName(target.value)}
+            />
           </div>
 
           <div class="mb-3">
@@ -22,7 +42,9 @@ export default function Contact() {
               type="email"
               class="form-control"
               id="email"
+              name="email"
               aria-describedby="emailHelp"
+              onChange={({ target }) => setEmail(target.value)}
             />
           </div>
 
@@ -30,7 +52,13 @@ export default function Contact() {
             <label for="message" class="form-label">
               Message
             </label>
-            <textarea class="form-control" id="message" rows="3"></textarea>
+            <textarea
+              class="form-control"
+              id="message"
+              name="message"
+              rows="3"
+              onChange={({ target }) => setMessage(target.value)}
+            ></textarea>
           </div>
           <button type="submit" class="btn btn-primary">
             Submit
